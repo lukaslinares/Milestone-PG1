9bcfb2ce622d4d477e82a0e1db6ad6e8c7977e20
lukaslinares <lukaslinares12@gmail.com>
2022-10-18 16:57:59 -0700

    Check for match

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ class AudioController{
         this.matchSound = new Audio('Assets/Sounds/match.mp3');
         this.victorySound = new Audio('Assets/Sounds/victory.wav');
         this.gameOverSound = new Audio('Assets/Sounds/gameOver.mp3');
-        this.bgMusic.volume = 0.1;
-        this.bgMusic.loop = true; 
+        this.bgMusic.volume = 0.2;
         this.gameOverSound.volume = 0.3;
+        this.victorySound.volume = 0.1;
     }
     startMusic(){
         this.bgMusic.play();
@@ -73,6 +73,49 @@ class Nintendo{
                 else
                     this.cardToCheck = card;
         }
+    }
+    checkForCardMatch(card){
+        if(this.getCardType(card) === this.getCardType(this.cardToCheck))
+            this.cardMatch(card, this.cardToCheck);
+        else 
+            this.notAMatch(card, this.cardToCheck);
+        this.cardToCheck = null;
+    }
+    cardMatch(card1, card2){
+        this.matchedCards.push(card1);
+        this.matchedCards.push(card2);
+        this.audioController.match();
+        if(this.matchedCards.length === this.cardsArray.length)
+            this.victory();
+    }  
+    notAMatch(card1, card2){
+        this.busy = true;
+        setTimeout(()=>{
+            card1.classList.remove('visible')
+            card2.classList.remove('visible')
+            this.busy = false;
+        }, 800);
+    }
+    getCardType(card){
+        return card.getElementsByClassName('value')[0].src;
+    }  
+    startCountdown(){
+        return setInterval(()=>{
+            this.timeRemaining --;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+                this.gameOver();
+        },1000);
+    }
+    victory(){
+       clearInterval(this.countDown)
+       this.audioController.victory();
+       document.getElementById('victory-text').classList.add('visible') 
+    }
+    gameOver(){
+        clearInterval(this.countDown);
+        this.audioController.gameOver();
+        document.getElementById('game-over-text').classList.add('visible');
     }
     canFlipCard(card){
         return true;
